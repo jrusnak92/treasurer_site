24b5248e97f4b182c1bea1e6232eddccf64bbb92
Jonathan Rusnak <jrusnak@usc.edu>
2013-08-08 20:37:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreasurerSitev1::Application.config.secret_token = '83302f470159e6bb68af5380a031792872c53bc487827efbbb6a075ac52b1c3366ae8ead75e91b1722198c05b5023e4d37c1c26b8d50b1d8967d5c8820e68ce6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+TreasurerSitev1::Application.config.session_store :cookie_store, key: '_treasurer_siteV1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# TreasurerSitev1::Application.config.session_store :active_record_store
